021dd6d578833dde3d323e6b300435a9eebacfb6
Rujula-Bhatkar <iitjee2025aim@gmail.com>
2025-05-07 17:05:23 +0530

    Add files via upload

---
+++ A1T1.py
@@ -0,0 +1,15 @@
+#ASSIGNMENT 1 TASK 1:
+
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the first number: "))
+
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1/n2
+
+print("Addition: ",add)
+print("Subtraction: ",sub)
+print("Multiplication: ",mul)
+print("Division: ",div)
+
---
+++ A2T2.py
@@ -0,0 +1,7 @@
+#ASSIGNMENT 1 TASK 2:
+f_name=input("Enter your first name: ")
+l_name=input("Enter your last name: ")
+
+name=f_name+" "+l_name
+
+print("Hello, ",name,"! Welcome to the Python program.")
